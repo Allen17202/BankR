895e013de7a1c4d3c90d6dfcf71dc27e962501ed
Gregory Allen <traquanallen.gtta@knights.ucf.edu>
2020-11-28 13:56:51 -0500

    Added Accounts, Tranfer and Record apps

---
+++ transactionrecord/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class TransactionrecordConfig(AppConfig):
+    name = 'transactionrecord'
---
+++ transferfunds/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class TransferfundsConfig(AppConfig):
+    name = 'transferfunds'
