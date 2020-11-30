cac6ec742f3b62473db48d760bac6c6cd7b6a3f7
Gregory Allen <traquanallen.gtta@knights.ucf.edu>
2020-11-29 20:27:49 -0500

    Added Login and Logout, also finished creating home page

--- BankR/settings.py
+++ BankR/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'accounts.apps.AccountsConfig',
+    'transactionrecord.apps.TransactionrecordConfig',
+    'transferfunds.apps.TransferfundsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
 
-# Create your models here.
+class AccountInfo(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    accountNumber = models.IntegerField()
+    CheckingBalance = models.IntegerField()
+    SavingBalance = models.IntegerField()
